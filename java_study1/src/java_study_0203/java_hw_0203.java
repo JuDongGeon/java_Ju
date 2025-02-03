04e1aa8ae9aec8e02c4d5439f9b88e357e289f76
JuDongGeon <ehdrjs981024@gmail.com>
2025-02-03 12:48:12 +0900

    250203 자바수업 및 과제

---
+++ java_study1/src/java_study_0203/java_hw_0203.java
@@ -0,0 +1,26 @@
+package java_study_0203;
+
+import java.util.Scanner;
+
+public class java_hw_0203 {
+
+	public static void main(String[] args) {
+		// 2월 3일 과제
+
+		
+		
+		/* 가위바위보 게임 만들기(Math.randon(); => 랜덤 사용하기)
+		 * 1.가위 2.바위 3.보
+		 * 내가 이겼는지 졌는지 비겼는지 출력
+		 */
+		
+		Scanner game = new Scanner(System.in);
+		
+		int user = 0, com = 0;
+		
+		
+		
+
+	}
+
+}

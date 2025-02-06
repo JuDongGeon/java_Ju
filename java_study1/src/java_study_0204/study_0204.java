969f96e67d9adb4ea2a6cd0ed669d012f4e22e77
JuDongGeon <ehdrjs981024@gmail.com>
2025-02-06 12:50:43 +0900

    250206 수업내용 및 과제

---
+++ java_study1/src/java_study_0204/study_0204.java
@@ -0,0 +1,39 @@
+package java_study_0204;
+
+import java.util.Scanner;
+
+public class study_0204 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for(int i = 1; i<= 10; i += 2) {
+			System.out.println(i);
+		}
+
+		Scanner scan = new Scanner(System.in);
+		System.out.print("숫자 입력 : ");
+		int num = scan.nextInt();
+		
+		for (int i1 = num; i1>= 1; i1--) {
+			System.out.println("감소 반복!");
+		}
+		
+		System.out.print("숫자 입력 : ");
+		int num1 = scan.nextInt();
+		
+		for (int i2 = 1; i2 <= num1; i2++) {
+			System.out.println("증가 반복!");
+		}
+		
+		// for 문을 만드는 방법
+		// for를 입력 후 조건식 (){}순으로 입력
+		// ()안에는 우선 시작한 값 설정 ex) int i = 3 => i 값을 3부터 시작한다는 의미
+		// 두번쨰로 범위 설정 ex) i <=(또는 >=, <, > 다 상관없음) num (값을 넣어도 설정해도 되고 입력값을 받아와도 됨) => num 보다 i값이 작으면 실행
+		// 마지막으로 i의 증감을 표시 ex) i++(또는 i += 1)
+		
+		
+	}
+
+}
+                                               
---
+++ java_study1/src/java_study_0206/study_0206.java
@@ -0,0 +1,49 @@
+package java_study_0206;
+
+import java.util.Scanner;
+
+public class study_0206 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		Scanner sc = new Scanner(System.in);
+		
+		for(int i = 1; i <=  3; i++) {
+			for(int k = 1; k<= 2; k++) {
+				System.out.println("i= " + i + "  k = " + k);
+			}
+		}
+		
+		int i1 = 1;
+		while(i1 <= 3) {
+			int k1 = 1;
+			while (k1 <= 2) {
+				System.out.println("i1 = " + i1 + "  k1 = " + k1);
+				k1++;
+			}
+			i1++;
+		}
+		
+		int a = 10;
+		do {
+			System.out.println("do ~ while문");
+			a++;
+		}while(a<15); // for 와 while 과 다르게 do while은 조건식 뒤에 ;을 붙혀야함
+		
+		/* 
+		 * do while에 프로세스
+		 * 초기값 확인 -> {내용} 실행 -> 조건식 비교 -> 참일결우 {내용} 실행 -> 증감 실행 -> 조건식 비교 -> 참일경우 {내용} 실행(거짓일때 까지 반복) 
+		 */
+ 
+		// 예제 : 가위바위보
+		int user = 0;
+		do {
+			System.out.print("1.가위 2.바위 3.보 :");
+			user = sc.nextInt(); 
+		} while (!(user >= 1 && user <= 3));
+		// 반복문의 조건식과 내용에 의해서 만들어진 데이터가 연관있다면 do while 을 사용하는 것을 생각해보아야함
+	}
+
+}

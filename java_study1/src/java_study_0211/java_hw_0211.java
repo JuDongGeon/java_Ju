2a3e5f3d12beffdda3808418b36acf8c4fc1dbef
JuDongGeon <ehdrjs981024@gmail.com>
2025-02-11 13:56:09 +0900

    250211 자바 수업 및 과제

---
+++ java_study1/src/java_study_0211/java_hw_0211.java
@@ -0,0 +1,49 @@
+package java_study_0211;
+
+import java.util.Scanner;
+
+public class java_hw_0211 {
+
+	public static void main(String[] args) {
+		// 250211 과제
+		
+		/*5줄 빙고판 만들기
+		 * 중복으로 숫자가 들어가지 않는 5*5 빙고판 만들기
+		 * 5줄 빙고가 되었을때 게임이 끝나도록 빙고판 만들기
+		 * 기본틀은 study02_0211 클래스 파일 팜고
+		 */
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int [] bingo = new int [25]; // 빙고 배열
+		
+		for (int i = 0; i < bingo.length; i++) {
+			int num = (int)(Math.random()*50+1);
+			boolean isSame = false;
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		// 시간이 남으면 컴퓨터와 함께하는 2인용 제작
+		
+		
+	}
+
+}
